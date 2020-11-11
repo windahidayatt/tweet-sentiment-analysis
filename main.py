7d8a36ac9767f9b58ba934b4cd28e2a74a0f87cd
windahidayatt <windahidayat27@gmail.com>
2020-11-11 16:16:01 +0700

    (#) New file structure

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import modul_sentimen
 
-train = modul_sentimen.set_data_train()
-test = modul_sentimen.set_data_test()
+language = 'english'
 
+train = modul_sentimen.set_data_train(language)
+test = modul_sentimen.set_data_test(language)
+    
 if(modul_sentimen.check_train_tidy_tweets(train) == False):
-    train = modul_sentimen.train_text_preprocessing(test)
+    train = modul_sentimen.train_text_preprocessing(train, language)
 else : 
     print("no train text prepocessing")
 
-test = modul_sentimen.test_text_preprocessing(test)
+test = modul_sentimen.test_text_preprocessing(test, language)
 
 combine = modul_sentimen.combine_data(train, test)
 
-train_tfidf_matrix = modul_sentimen.word_embedding(train, combine)
+train_tfidf_matrix = modul_sentimen.word_embedding(train, combine, language)
 
 x_train_tfidf, x_valid_tfidf, y_train_tfidf, y_valid_tfidf = modul_sentimen.split_data(train, train_tfidf_matrix[1])
 
 Log_Reg = modul_sentimen.logistic_regression_model(x_train_tfidf, x_valid_tfidf, y_train_tfidf, y_valid_tfidf)
-modul_sentimen.set_predict_with_model(train, test, train_tfidf_matrix[0], Log_Reg)
+modul_sentimen.set_predict_with_model(train, test, train_tfidf_matrix[0], Log_Reg, language)
 
 modul_sentimen.show_accuracy(x_valid_tfidf, y_valid_tfidf, Log_Reg)
--- modul_sentimen.py
+++ modul_sentimen.py
@@ -10,6 +10,7 @@ from os import listdir
 from os.path import isfile, join
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer 
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
@@ -17,9 +18,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report
 
 
-def set_data_train():
+def set_data_train(language):
     '''
     Set the train data.
+
+    Args
+    ----------
+    language : string
+        a string of the language that choosen (english or indonesia)
 
     Returns
     ----------
@@ -28,16 +34,21 @@ def set_data_train():
     '''
 
     #Choose data train file and store it in train data frame
-    train = pd.read_csv('./data_train/result.csv', error_bad_lines=False, index_col=False, dtype='unicode')
+    train = pd.read_csv('./data_train/train_' + language + '.csv', error_bad_lines=False, index_col=False, dtype='unicode')
     
     #Remove nan value
-    train.dropna(subset = ["sentimen"], inplace=True)
+    train.dropna(subset = ["Sentiment"], inplace=True)
 
     return train
 
-def set_data_test():
+def set_data_test(language):
     '''
     Set the test data.
+
+    Args
+    ----------
+    language : string
+        a string of the language that choosen (english or indonesia)
 
     Returns
     ----------
@@ -46,7 +57,7 @@ def set_data_test():
     '''
 
     #Take all the file names from folder data_test
-    mypath = "./data_test/"
+    mypath = "./data_test/" + language + "/"
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
     #Open the data test file by it names (which the name was already store in onlyfiles) and store it in test data frame
@@ -58,6 +69,10 @@ def set_data_test():
         else :
             test = test.append(temp, ignore_index=True, sort=True)
         i = i+1
+    
+    #define the name of the column to test and rename it to Text
+    column_name = 'tweet'
+    test = test.rename(columns = {column_name: 'Text'}, inplace = False)
     
     return test
 
@@ -87,9 +102,14 @@ def remove_pattern(text,pattern):
     
     return text
 
-def set_stopwords():
+def set_stopwords(language):
     '''
     Set the list of stopwords.
+
+    Args
+    ----------
+    language : string
+        a string of the language that choosen (english or indonesia)
 
     Returns
     ----------
@@ -97,8 +117,11 @@ def set_stopwords():
         a list of stopwords in indonesian
     '''
 
-    #Define stopwords in indonesian (Using nltk)
-    list_stopword =  set(stopwords.words('indonesian'))
+    #Define stopwords in english (Using nltk)
+    if language == 'indonesia' :
+        language = 'indonesian'
+
+    list_stopword =  set(stopwords.words(language))
 
     list_stopwords = []
 
@@ -108,7 +131,7 @@ def set_stopwords():
 
     return list_stopwords
 
-def remove_stopwords(text):
+def remove_stopwords(text, language):
     '''
     Remove stopword(s) e.g. "yang", "di", "ke", etc.
 
@@ -116,6 +139,8 @@ def remove_stopwords(text):
     ----------
     text : string
         a string that will be processed to remove the stopword(s)
+    language : string
+        a string of the language that choosen (english or indonesia)
 
     Returns
     ----------
@@ -124,7 +149,7 @@ def remove_stopwords(text):
     '''
 
     #Define stopwords in indonesian (Using nltk)
-    list_stopwords = set_stopwords()
+    list_stopwords = set_stopwords(language)
 
     removed = []
     text = text.translate(str.maketrans('','',string.punctuation)).lower()
@@ -137,9 +162,14 @@ def remove_stopwords(text):
 
     return text
 
-def set_stremmer():
+def set_stremmer(language):
     '''
     Set the stemmer (using sastrawi).
+
+    Args
+    ----------
+    language : string
+        a string of the language that choosen (english or indonesia)
 
     Returns
     ----------
@@ -147,13 +177,18 @@ def set_stremmer():
         a stemmer to use for stemming the data
     '''
 
-    #Create stemmer for stemming bahasa indonesia using sastrawi    
-    factory = StemmerFactory()
-    stemmer = factory.create_stemmer()
+    #Check the language
+    if language == 'english' :
+        #Create stemmer for stemming english using nltk
+        stemmer = PorterStemmer() 
+    elif language == 'indonesia' :
+        #Create stemmer for stemming bahasa indonesia using sastrawi
+        factory = StemmerFactory()
+        stemmer = factory.create_stemmer()
 
     return stemmer
 
-def stemming_sentence(text):
+def stemming_sentence(text, language):
     '''
     Stemming each word of the text.
 
@@ -161,6 +196,8 @@ def stemming_sentence(text):
     ----------
     text : string
         a string that will be processed to stemming
+    language : string
+        a string of the language that choosen (english or indonesia)
 
     Returns
     ----------
@@ -169,14 +206,21 @@ def stemming_sentence(text):
     '''
 
     #Get the stemmer
-    stemmer = set_stremmer()
+    stemmer = set_stremmer(language)
 
-    text = stemmer.stem(text)
+    stemmed = []
+    text = text.translate(str.maketrans('','',string.punctuation)).lower()
+    tokens = word_tokenize(text)
+    for t in tokens:
+        stemmed.append(stemmer.stem(t))
+            
+    text = " ".join(stemmed)
+
     return text
 
 def check_train_tidy_tweets(train):
     '''
-    Check is the train data has a column that named 'Tidy_Tweets'.
+    Check is the train data has a column that named 'Tidy_Text'.
     This column contains the tweet in train data that already passed the text preprocessing.
 
     Args
@@ -186,24 +230,26 @@ def check_train_tidy_tweets(train):
     Returns
     ----------
     boolean
-        a boolean is there a 'Tidy_Tweets' column
+        a boolean is there a 'Tidy_Text' column
 
     '''
     #if the first time this module was run and the train data not yet passed the text preprocessing
-    if 'Tidy_Tweets' in train.columns:
+    if 'Tidy_Text' in train.columns:
         return True
     else :
         return False
 
-def train_text_preprocessing(train):
+def train_text_preprocessing(train, language):
     '''
     Text preprocessing (remove handler, remove all character(s) except a-z/A-Z, remove stopwords, stemming) the train data.
-    This function only run once in case the training data not yet passed the text preprocessing (doesn't have a Tidy_Tweets column).
+    This function only run once in case the training data not yet passed the text preprocessing (doesn't have a Tidy_Text column).
 
     Args
     ----------
     train : pandas.DataFrame
         a data frame of the train data
+    language : string
+        a string of the language that choosen (english or indonesia)
 
     Returns
     ----------
@@ -212,14 +258,14 @@ def train_text_preprocessing(train):
     '''
 
     #Text Preprocessing data train
-    train['Tidy_Tweets'] = np.vectorize(remove_pattern)(train['Tweet'], "@[\rw]*")
-    train['Tidy_Tweets'] = train['Tidy_Tweets'].str.replace("[^a-zA-Z#]", " ")
-    train['Tidy_Tweets'] = np.vectorize(remove_stopwords)(train['Tidy_Tweets'])
-    train['Tidy_Tweets'] = np.vectorize(stemming_sentence)(train['Tidy_Tweets'])
+    train['Tidy_Text'] = np.vectorize(remove_pattern)(train['Text'], "@[\\w]*")
+    train['Tidy_Text'] = train['Tidy_Text'].str.replace("[^a-zA-Z#]", " ")
+    train['Tidy_Text'] = np.vectorize(remove_stopwords)(train['Tidy_Text'], language)
+    train['Tidy_Text'] = np.vectorize(stemming_sentence)(train['Tidy_Text'], language)
 
     return train
 
-def test_text_preprocessing(test):
+def test_text_preprocessing(test, language):
     '''
     Text preprocessing (remove handler, remove all character(s) except a-z/A-Z, remove stopwords, stemming) the test data.
 
@@ -227,6 +273,8 @@ def test_text_preprocessing(test):
     ----------
     test : pandas.DataFrame
         a data frame of the test data
+    language : string
+        a string of the language that choosen (english or indonesia)
 
     Returns
     ----------
@@ -234,13 +282,13 @@ def test_text_preprocessing(test):
         a data frame of the test data that already passed the text preprocessing
     '''
 
-    test = test.rename(columns = {'tweet': 'Tweet'}, inplace = False)
+    # test = test.rename(columns = {'tweet': 'Text'}, inplace = False)
 
     #Text Preprocessing data test
-    test['Tidy_Tweets'] = np.vectorize(remove_pattern)(test['Tweet'], "@[\rw]*")
-    test['Tidy_Tweets'] = test['Tidy_Tweets'].str.replace("[^a-zA-Z#]", " ")
-    test['Tidy_Tweets'] = np.vectorize(remove_stopwords)(test['Tidy_Tweets'])
-    test['Tidy_Tweets'] = np.vectorize(stemming_sentence)(test['Tidy_Tweets'])
+    test['Tidy_Text'] = np.vectorize(remove_pattern)(test['Text'], "@[\rw]*")
+    test['Tidy_Text'] = test['Tidy_Text'].str.replace("[^a-zA-Z#]", " ")
+    test['Tidy_Text'] = np.vectorize(remove_stopwords)(test['Tidy_Text'], language)
+    test['Tidy_Text'] = np.vectorize(stemming_sentence)(test['Tidy_Text'], language)
 
     return test
 
@@ -265,7 +313,7 @@ def combine_data(train, test):
 
     return combine
 
-def word_embedding(train, combine):
+def word_embedding(train, combine, language):
     '''
     Word embedding with TF-IDF method.
 
@@ -275,22 +323,24 @@ def word_embedding(train, combine):
         a data frame of the train data
     combine : pandas.DataFrame
         a data frame of the data that the result of a combine from data train and data test
+    language : string
+        a string of the language that choosen (english or indonesia)
 
     Returns
     ----------
     tfidf_matrix : scipy.sparse.csr.csr_matrix
-        a csr_matrix of the Tidy_Tweets column from combine data
+        a csr_matrix of the Tidy_Text column from combine data
     train_tfidf_matrix : scipy.sparse.csr.csr_matrix
-        a csr_matrix of the Tidy_Tweets column from train data
+        a csr_matrix of the Tidy_Text column from train data
     '''
 
     #Word Embedding (TF IDF)
-    list_stopwords = set_stopwords()
+    list_stopwords = set_stopwords(language)
 
     tfidf=TfidfVectorizer(max_df=0.90, min_df=2,max_features=1000,stop_words=frozenset(list_stopwords))
     # tfidf=TfidfVectorizer(max_df=0.90, min_df=2,max_features=1000,stop_words=frozenset(stopwords))
 
-    tfidf_matrix=tfidf.fit_transform(combine['Tidy_Tweets'].values.astype('U'))
+    tfidf_matrix=tfidf.fit_transform(combine['Tidy_Text'].values.astype('U'))
 
     # df_tfidf = pd.DataFrame(tfidf_matrix.todense())
 
@@ -307,7 +357,7 @@ def split_data(train, train_tfidf_matrix):
     train : pandas.DataFrame
         a data frame of the train data
     train_tfidf_matrix : csr_matrix
-        a csr_matrix of the Tidy_Tweets column from train data
+        a csr_matrix of the Tidy_Text column from train data
 
     Returns
     ----------
@@ -316,13 +366,13 @@ def split_data(train, train_tfidf_matrix):
     x_valid_tfidf : scipy.sparse.csr.csr_matrix
         a csr_matrix that the value is randomly filled from train_tfidf_matrix
     y_train_tfidf : scipy.sparse.csr.csr_matrix
-        a csr_matrix that the value is randomly filled from sentimen column in data train
+        a csr_matrix that the value is randomly filled from sentiment column in data train
     y_valid_tfidf : scipy.sparse.csr.csr_matrix
-        a csr_matrix that the value is randomly filled from sentimen column in data train
+        a csr_matrix that the value is randomly filled from sentiment column in data train
     '''
 
     #Split the training data
-    x_train_tfidf, x_valid_tfidf, y_train_tfidf, y_valid_tfidf = train_test_split(train_tfidf_matrix,train['sentimen'],test_size=0.3,random_state=17)
+    x_train_tfidf, x_valid_tfidf, y_train_tfidf, y_valid_tfidf = train_test_split(train_tfidf_matrix,train['Sentiment'],test_size=0.3,random_state=17)
 
     return (x_train_tfidf, x_valid_tfidf, y_train_tfidf, y_valid_tfidf)
 
@@ -337,9 +387,9 @@ def logistic_regression_model(x_train_tfidf, x_valid_tfidf, y_train_tfidf, y_val
     x_valid_tfidf : scipy.sparse.csr.csr_matrix
         a csr_matrix that the value is randomly filled from train_tfidf_matrix
     y_train_tfidf : scipy.sparse.csr.csr_matrix
-        a csr_matrix that the value is randomly filled from sentimen column in data train
+        a csr_matrix that the value is randomly filled from sentiment column in data train
     y_valid_tfidf : scipy.sparse.csr.csr_matrix
-        a csr_matrix that the value is randomly filled from sentimen column in data train
+        a csr_matrix that the value is randomly filled from sentiment column in data train
 
     Returns
     ----------
@@ -355,7 +405,7 @@ def logistic_regression_model(x_train_tfidf, x_valid_tfidf, y_train_tfidf, y_val
 
     return Log_Reg
 
-def set_predict_with_model(train, test, tfidf_matrix, Log_Reg):
+def set_predict_with_model(train, test, tfidf_matrix, Log_Reg, language):
     '''
     Predict the result using Logistic Regression model.
 
@@ -366,20 +416,22 @@ def set_predict_with_model(train, test, tfidf_matrix, Log_Reg):
     test : pandas.DataFrame
         a data frame of the testing data
     tfidf_matrix : scipy.sparse.csr.csr_matrix
-        a csr_matrix of the Tidy_Tweets column from combined data
+        a csr_matrix of the Tidy_Text column from combined data
     Log_Reg : sklearn.linear_model._logistic.LogisticRegression
         a logistic regression model to use for predict the result
+    language : string
+        a string of the language that choosen (english or indonesia)
     '''
 
     test_tfidf = tfidf_matrix[len(train):]
 
-    test['sentimen'] = Log_Reg.predict(test_tfidf)
+    test['Sentiment'] = Log_Reg.predict(test_tfidf)
 
     combine = train.append(test,ignore_index=True,sort=True)
 
-    store_to_csv(test, combine)
+    store_to_csv(test, combine, language)
 
-def store_to_csv(test, combine):
+def store_to_csv(test, combine, language):
     '''
     Store the result of the test data and combined data with sentiment to local csv
 
@@ -389,18 +441,19 @@ def store_to_csv(test, combine):
         a data frame of the test data
     combine : pandas.DataFrame
         a data frame of the data that the result of a combine from data train and data test
+    language : string
+        a string of the language that choosen (english or indonesia)
     '''
 
     #Store all combine data to csv
-    combine_submission = combine[['Tweet', 'sentimen', 'Tidy_Tweets']]
-    combine_submission.to_csv('./data_train/result.csv', index=False)
+    combine_submission = combine[['Text', 'Sentiment', 'Tidy_Text']]
+    combine_submission.to_csv('./data_train/train_' + language + '.csv', index=False)
 
-    # test = test.drop('Tidy_Tweets', axis=1)
-    test = test.rename(columns = {'Tweet': 'tweet'}, inplace = False)
+    test = test.drop('Tidy_Text', axis=1)
 
-    #Store the test data result (with sentimen) to csv
+    #Store the test data result (with sentiment) to csv
     # submission = test
-    test.to_csv('./data_train/result_ikm.csv', index=False)
+    test.to_csv('./data_result/result_' + language + '.csv', index=False)
 
 def show_accuracy(x_valid_tfidf, y_valid_tfidf, Log_Reg):
     '''
@@ -411,38 +464,9 @@ def show_accuracy(x_valid_tfidf, y_valid_tfidf, Log_Reg):
     x_valid_tfidf : scipy.sparse.csr.csr_matrix
         a csr_matrix that the value is randomly filled from train_tfidf_matrix
     y_valid_tfidf : scipy.sparse.csr.csr_matrix
-        a csr_matrix that the value is randomly filled from sentimen column in data train
+        a csr_matrix that the value is randomly filled from sentiment column in data train
     Log_Reg : sklearn.linear_model._logistic.LogisticRegression
         a logistic regression model to use for predict the result
     '''
     y_pred = Log_Reg.predict(x_valid_tfidf)
     print(classification_report(y_valid_tfidf, y_pred))
-    
-def main():
-    train = set_data_train()
-    test = set_data_test()
-    
-    if(check_train_tidy_tweets(train) == False):
-        train = train_text_preprocessing(test)
-    else : 
-        print("no train text prepocessing")
-
-    test = test_text_preprocessing(test)
-
-    combine = combine_data(train, test)
-
-    train_tfidf_matrix = word_embedding(train, combine)
-
-    x_train_tfidf, x_valid_tfidf, y_train_tfidf, y_valid_tfidf = split_data(train, train_tfidf_matrix[1])
-
-    Log_Reg = logistic_regression_model(x_train_tfidf, x_valid_tfidf, y_train_tfidf, y_valid_tfidf)
-    set_predict_with_model(train, test, train_tfidf_matrix[0], Log_Reg)
-
-    show_accuracy(x_valid_tfidf, y_valid_tfidf, Log_Reg)
-
-    # print(y_train_tfidf)
-    # print(remove_stopwords.__doc__)
-
-
-if __name__ == "__main__":
-    main()
